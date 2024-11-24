eb514ae3817ec749592c271a0562cb1a30a9e897
Emmanuel Omoiya <emmanuelomoiya6@gmail.com>
2024-11-24 05:25:12 +0100

    feat: bulk mail send

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ new Worker(
       const templateDir = path.join(process.cwd(), "./templates");
       const source = fs.readFileSync(path.join(templateDir, html), "utf-8");
       const compiledTemplate = handlebars.compile(String(source));
+      console.log(compiledTemplate)
 
       // Send email
       const info = await transporter.sendMail({
